c2084f06eefae23b078a4fec638241964b14622a
Andres Tobio <92232669+AndrewUru@users.noreply.github.com>
2025-07-08 00:35:26 +0200

    modificando server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,19 +1,17 @@
 // lib/supabase/server.ts
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 
 export function createClient() {
-  const cookieStore = cookies(); // ✅ NO lleva await
+  const cookieStore = cookies(); // ✅ No es async, devuelve directamente ReadonlyRequestCookies
 
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        get: (key: string) => {
-          const cookie = cookieStore.get(key); // ✅ Aquí tampoco hay await
-          return cookie?.value;
-        },
+        get: (key: string) => cookieStore.get(key)?.value,
       },
     }
   );
